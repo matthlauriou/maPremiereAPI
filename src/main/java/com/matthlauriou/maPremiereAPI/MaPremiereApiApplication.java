8e9ed8f19b04d88be254e6048ba8ec63ef072698
Matth <63979384+matthlauriou@users.noreply.github.com>
2023-10-26 18:15:21 +0200

    MLA init project

---
+++ src/main/java/com/matthlauriou/maPremiereAPI/MaPremiereApiApplication.java
@@ -0,0 +1,13 @@
+package com.matthlauriou.maPremiereAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaPremiereApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaPremiereApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matthlauriou/maPremiereAPI/MaPremiereApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthlauriou.maPremiereAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaPremiereApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
